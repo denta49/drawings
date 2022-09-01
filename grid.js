82fa0c6b11421d8c930a02a6815d74eb353f95e1
p.wojenka <p.wojenka@361.sh>
2022-09-01 12:58:05 +0200

    install tweakpane

--- grid.js
+++ grid.js
@@ -1,22 +1,26 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [2048, 2048],
+  animate: true,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 4;
-    const rows = 3;
+    const cols = 10;
+    const rows = 10;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
     const gridh = height * 0.8;
     const cellw = gridw / cols;
     const cellh = gridh / rows;
+
     const margx = (width - gridw) * 0.5;
     const margy = (height - gridh) * 0.5;
 
@@ -28,13 +32,17 @@ const sketch = () => {
       const y = row * cellh;
       const w = cellw * 0.8;
       const h = cellh * 0.8;
+      const n = random.noise2D(x + frame * 10, y, 0.001);
+      const angle = n * Math.PI * 0.2;
+      const scale = math.mapRange(n, -1, 1, 1, 30);
 
       context.save();
       context.translate(x, y);
       context.translate(margx, margy);
       context.translate(cellw * 0.5, cellh * 0.5);
+      context.rotate(angle);
 
-      context.lineWidth = 4;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
